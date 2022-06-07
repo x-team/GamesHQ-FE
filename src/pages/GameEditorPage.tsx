d3adebbc2c06859471b8a9782dd85558a09d2bf2
Bernardo Rubín <bernardorubin@gmail.com>
2022-06-06 21:26:51 -0500

    Fix crash

--- src/pages/GameEditorPage.tsx
+++ src/pages/GameEditorPage.tsx
@@ -93,7 +93,7 @@ const GameEditorPage = function GameEditorPage({ editMode }: IProps) {
           clientSecret: gameType.clientSecret,
           signingSecret: gameType.signingSecret,
           _createdById: gameType._createdById,
-          _leaderboards : gameType._leaderboards
+          _leaderboards: gameType._leaderboards,
         } as IForm);
 
         setGameType(gameType);
@@ -180,15 +180,21 @@ const GameEditorPage = function GameEditorPage({ editMode }: IProps) {
               </th>
               <th className="bg-gray-100 border text-left px-8 py-4">Edit</th>
             </tr>
-            {currentGameType?._leaderboards.map((leaderboard: ILeaderboard) => (
-              <tr>
-                <td className="border px-8 py-4">{leaderboard.id}</td>
-                <td className="border px-8 py-4">{leaderboard.name}</td>
-                <td className="border px-8 py-4">{leaderboard.scoreStrategy}</td>
-                <td className="border px-8 py-4">{leaderboard.resetStrategy}</td>
-                <td className="border px-8 py-4">"EDIT Button TBD"</td>
-              </tr>
-            ))}
+            {currentGameType?._leaderboards?.map(
+              (leaderboard: ILeaderboard) => (
+                <tr>
+                  <td className="border px-8 py-4">{leaderboard.id}</td>
+                  <td className="border px-8 py-4">{leaderboard.name}</td>
+                  <td className="border px-8 py-4">
+                    {leaderboard.scoreStrategy}
+                  </td>
+                  <td className="border px-8 py-4">
+                    {leaderboard.resetStrategy}
+                  </td>
+                  <td className="border px-8 py-4">"EDIT Button TBD"</td>
+                </tr>
+              )
+            )}
           </table>
         </div>
       </>
