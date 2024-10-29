f6ce5f2e15a6bb49d19dc8ab8f3fd9cecd29df1a
Nick Plekhanov <nick.plekhanov@x-team.com>
2024-10-29 19:55:17 +0100

    Create unit tests for PanelBox component

--- src/ui/PanelBox.tsx
+++ src/ui/PanelBox.tsx
@@ -1,16 +1,18 @@
+import React from 'react'
+
 interface IProps {
-    children: React.ReactNode;
-    className?: any;
+  children: React.ReactNode
+  className?: any
 }
 
 function PanelBox(props: IProps) {
-    return (
-        <span
-            className={`p-6 bg-white rounded-xl border border-gray-200 flex flex-col ${props.className}`}
-        >
-            {props.children}
-        </span>
-    );
+  return (
+    <span
+      className={`p-6 bg-white rounded-xl border border-gray-200 flex flex-col ${props.className}`}
+    >
+      {props.children}
+    </span>
+  )
 }
 
-export default PanelBox;
+export default PanelBox
