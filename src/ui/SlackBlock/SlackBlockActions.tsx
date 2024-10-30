72f7ab287c5128bc23ee686edbabe6eb9e9f2e2a
Nick Plekhanov <nick.plekhanov@x-team.com>
2024-10-30 02:14:12 +0100

    Create unit tests for SlackBlockActions component

--- src/ui/SlackBlock/SlackBlockActions.tsx
+++ src/ui/SlackBlock/SlackBlockActions.tsx
@@ -1,28 +1,40 @@
+import React from 'react'
 
-import { SlackBlockStaticSelect } from ".";
- import { SlackBlockKitSelectMenuElement, SlackBlockKitButtonElement } from '../../SlackBlockKit';
-import SlackBlockButton from "./SlackBlockButton";
+import { SlackBlockStaticSelect } from '.'
+import {
+  SlackBlockKitSelectMenuElement,
+  SlackBlockKitButtonElement
+} from '../../SlackBlockKit'
+import SlackBlockButton from './SlackBlockButton'
 
 interface IProps {
-    elements: (SlackBlockKitSelectMenuElement | SlackBlockKitButtonElement)[]
-    onClose: () => void;
+  elements: (SlackBlockKitSelectMenuElement | SlackBlockKitButtonElement)[]
+  onClose: () => void
 }
-const SlackBlockActions = ({elements, onClose} : IProps ) => {
-    return (
-        <span>
-        {elements.map(e => {
-            if(e.type === 'static_select'){
-                return (
-                    <SlackBlockStaticSelect staticSelectElement={e} onClose={onClose}/>
-                )
-            }else{
-                return (
-                    <SlackBlockButton buttonElement={e} onClose={onClose}/>
-                )
-            }            
-        })}
-        </span>
-    )
-};
+const SlackBlockActions = ({ elements, onClose }: IProps) => {
+  return (
+    <span>
+      {elements.map(e => {
+        if (e.type === 'static_select') {
+          return (
+            <SlackBlockStaticSelect
+              key={e.action_id}
+              staticSelectElement={e}
+              onClose={onClose}
+            />
+          )
+        } else {
+          return (
+            <SlackBlockButton
+              key={e.action_id}
+              buttonElement={e}
+              onClose={onClose}
+            />
+          )
+        }
+      })}
+    </span>
+  )
+}
 
-export default SlackBlockActions;
+export default SlackBlockActions
